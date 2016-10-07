7a7f126acb646a908630b33438bad42020b69ef3
Thibault Deutsch <thibault.deutsch@gmail.com>
2016-10-07 22:08:42 +0200

    Add statico cmd

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # goutil
 
-Monolithic repository for Go packages
+Monolithic repository for Go packages and binaries.
+
+## Packages
+
+- `fs`: filesystem related
+
+
+## Binaries
+
+- `cmd/statico`: yet another simple static server
---
+++ cmd/statico/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+func LogRequest(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("[%s] %s - %s", r.Method, r.RemoteAddr, r.URL)
+		handler.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	listenAddr := flag.String("addr", ":80", "HTTP listen address")
+	staticDir := flag.String("dir", "/static/", "Static directory to serve")
+	flag.Parse()
+
+	fileServer := http.FileServer(http.Dir(*staticDir))
+	log.Fatal(http.ListenAndServe(*listenAddr, LogRequest(fileServer)))
+}
